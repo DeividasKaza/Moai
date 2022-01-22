c3c8c19b2e4e1fe3e6517f2064b90e9e932f44dd
Deividas Kazakevicius <90377794+DeividasKaza@users.noreply.github.com>
2022-01-22 18:49:40 +0100

    General Changes
    Moved over from storing the images on discord over to storing it locally, and formatted JS.

--- Chrome/code/content.js
+++ Chrome/code/content.js
@@ -1,25 +1,19 @@
 interaction = false;
-document.addEventListener('click', function () {
-interaction = true;
+document.addEventListener('click', function() {
+    interaction = true;
 });
 
 function play() {
     var audio = new Audio(chrome.runtime.getURL('sounds/sound.mp3'));
-    audio.volume = 1
-    audio.play();
-}
-
-document.body.innerHTML += '<img src="https://cdn.discordapp.com/attachments/713653773037469756/932692265900584980/Moai.png" id="Moai" style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);z-index:9999; width: 0rem;">';
-setInterval(sus, Math.floor(Math.random()* 10000) + 1000);
-
-function sus2(){
-    document.getElementById("Moai").style.width = "0rem";
-}
-
-function sus(){
     if (interaction) {
-    play();
-    document.getElementById("Moai").style.width = "50rem";
-    setTimeout(sus2, 5000);
+        audio.play();
+        document.getElementById("Moai").style.width = "50rem";
+        setTimeout(() => {
+            document.getElementById("Moai").style.width = "0rem";
+        }, 3000);
     }
 }
+
+image = chrome.runtime.getURL('images/Moai.png')
+document.body.innerHTML += `<img src="${image}" id="Moai" style="position:fixed;top:50%;left:50%;transform:translate(-50%,-50%);z-index:9999; width: 0rem;">`;
+setInterval(play, Math.floor(Math.random() * 10000) + 1000);
