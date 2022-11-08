1673f8c956e60b91790eff57703d70f14fba5677
Boop Dog <GoBoopADog@users.noreply.github.com>
2022-11-08 18:30:17 -0500

    V2
    Co-authored-by: Deividas Kazakevicius <90377794+DeividasKaza@users.noreply.github.com>

---
+++ code/background.js
@@ -0,0 +1,20 @@
+ let audio = new Audio(chrome.runtime.getURL('assets/audio/sound.ogg'));
+ let image = chrome.runtime.getURL('assets/images/Moai.png');
+
+document.addEventListener('click', function () {
+    let chance = Math.floor(Math.random() * 10);
+    console.log(chance);
+
+    if (chance == 0) {
+        audio.play();
+        let img = document.createElement('img');
+        img.src = image;
+ 
+        img.style = 'position: fixed; top: 50%; left: 50%; transform: translate(-50%, -50%); opacity: 0; transition: opacity 0s; z-index: 9999; width: 40%; height: auto; pointer-events: none;';
+        document.body.appendChild(img);
+        
+        setTimeout(function () { img.style.opacity = 1; }, 0);
+        setTimeout(function () { img.style.transition = 'opacity 1s'; img.style.opacity = 0; }, 500);
+        setTimeout(function () { img.remove(); }, 2000);
+    }
+})
